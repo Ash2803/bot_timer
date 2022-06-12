68e648118c8f80d0788eb699572e5c7d3863da0f
Ashot Abazyan <ashot.93@mail.ru>
2022-06-12 23:00:42 +0300

    Бот таймер v2, исправлен баг с прогресс баром

--- timer_bot.py
+++ timer_bot.py
@@ -27,7 +27,7 @@ def notify_progress(time, chat_id, message_id, total_time):
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
     iteration = min(total, iteration)
     percent = "{0:.1f}"
-    percent = percent.format(100 - (iteration * float(total)))
+    percent = percent.format(100 - (100 * (iteration / float(total))))
     filled_length = int(length * (total - iteration) // total)
     pbar = fill * filled_length + zfill * (length - filled_length)
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
