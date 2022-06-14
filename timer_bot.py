4653843c7c377c5e19073d4e2ae0058cb1b83fa8
Ash2803 <92327661+Ash2803@users.noreply.github.com>
2022-06-14 11:24:07 +0300

    Updated code structure

--- timer_bot.py
+++ timer_bot.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from pytimeparse import parse
 
+load_dotenv()
 TG_TOKEN = os.environ['TELEGRAM_TOKEN']  # подставьте свой ключ API
 TG_ID = os.environ['TG_CHAT_ID']  # подставьте свой ID
 bot = ptbot.Bot(TG_TOKEN)
@@ -31,12 +32,14 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
 
 
 def main():
-    load_dotenv()
-    bot.send_message(TG_ID, "Бот запущен")
-    bot.send_message(TG_ID, "Введите время")
-    bot.reply_on_message(reply)
-    bot.run_bot()
+  reply()
+  bot.send_message(TG_ID, "Бот запущен")
+  bot.send_message(TG_ID, "Введите время")
+  bot.run_bot()
 
 
 if __name__ == '__main__':
-    main()
+  bot.send_message(TG_ID, "Бот запущен")
+  bot.send_message(TG_ID, "Введите время")
+  bot.reply_on_message(reply)
+  bot.run_bot()
